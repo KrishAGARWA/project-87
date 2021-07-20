f30fa3157fc2d56416b178919414a6b6a5167bb9
KrishAGARWA <73591403+KrishAGARWA@users.noreply.github.com>
2021-07-20 21:12:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBbcBxQDhK2SXRsmJzj6OcCIYzdD5rzn4k",
+  authDomain: "spectrum-20e17.firebaseapp.com",
+  databaseURL: "https://spectrum-20e17-default-rtdb.firebaseio.com",
+  projectId: "spectrum-20e17",
+  storageBucket: "spectrum-20e17.appspot.com",
+  messagingSenderId: "64845663761",
+  appId: "1:64845663761:web:91ee5dfde1e817ed72abb0"
+};
